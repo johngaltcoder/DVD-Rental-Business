8db1101a39cbe4f886bcd038a331a36c8b25dfed
johngaltcoder <johngaltgithub@gmail.com>
2022-05-04 21:15:48 +0545

    Added identity by creating new project

---
+++ DVDRental/Controllers/MemberDetailsController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using DVDRental.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DVDRental.Controllers
+{
+    public class MemberDetails: Controller
+    {
+
+        private readonly ApplicationDbContext  _context;
+
+        public MemberDetails(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var ApplicationDbContext = _context.Member.OrderBy(m=>m.MemberFirstName).Include(m => m.MembershipCategory).ToList();
+            ApplicationDbContext.ForEach(m=>m.LoanCount = _context.Loan.Where(l=>l.MemberNumber == m.MemberNumber && l.DateReturned == null).ToList().Count);
+            return View(ApplicationDbContext.ToList());
+        }
+    }
+}
